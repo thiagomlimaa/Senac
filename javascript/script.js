2cb8dd1b34934bba8e6c0b9827a6ed2b68b8d4a2
Thiago Lima <thiagomlima2008@gmail.com>
2025-09-15 21:38:51 -0300

    JAVASCRIPT

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+document.write("<h1>Olá, mundo!</h1>");
+
+// console.log("Thiago, 17, Full-Stack")
+    console.log("Nome: Thiago Lima");
+    console.log("Idade: 17");
+    console.log("Curso: Full-Stack");
+
+    console.log("15.09.2025: " + new Date());
+
+    function mostrarMensagem() {
+        alert("Seja bem-vindo ao mundo do JavaScript!");
+      }
+
+
