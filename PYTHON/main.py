bfe8de27619044b2ee26d3da45c6e8c43c9607be
Thiago Lima <thiagomlima2008@gmail.com>
2025-10-29 17:26:10 -0300

    Create main.py

---
+++ PYTHON/main.py
@@ -0,0 +1,44 @@
+# Exercício 1:
+
+# nome = input("Qual seu nome? ")
+# idade = int(input("Qual sua idade? "))
+# cidade = input("Qual sua cidade? ")
+
+# print(f"Olá {nome}! você tem {idade} anos e mora em {cidade}.")
+
+
+
+
+
+
+# Exercício 2:
+# num1 = int(input("Digite o primeiro número: "))
+# num2 = int(input("Digite o segundo número: "))
+
+# soma = num1 + num2
+# subtracao = num1 - num2
+# div = num1 / num2
+# multi = num1 * num2
+
+# print(soma)
+# print(subtracao)
+# print(div)
+# print(multi)
+
+
+
+# Exercício 3:
+
+
+# celsius = float(input("Digite  Temperatura em Celsisus: "))
+
+# conversao = celsius * (9/5) + 32
+
+# print(f"{celsius}°C equivalem a {conversao}°F")
+
+
+
+
+
+
+
