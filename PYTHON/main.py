fc95d345e2ca0760e8512b57cd44d56c178a5f17
Thiago LIma <thirrasgomadeira@gmail.com>
2025-10-29 20:51:44 -0300

    Update main.py

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,44 +1,60 @@
-# Exercício 1:
-
-# nome = input("Qual seu nome? ")
-# idade = int(input("Qual sua idade? "))
-# cidade = input("Qual sua cidade? ")
-
-# print(f"Olá {nome}! você tem {idade} anos e mora em {cidade}.")
+# ATIVIDADE 1
 
 
+# num = int(input("Digite algum Númeor: "))
 
-
-
-
-# Exercício 2:
-# num1 = int(input("Digite o primeiro número: "))
-# num2 = int(input("Digite o segundo número: "))
-
-# soma = num1 + num2
-# subtracao = num1 - num2
-# div = num1 / num2
-# multi = num1 * num2
-
-# print(soma)
-# print(subtracao)
-# print(div)
-# print(multi)
-
-
-
-# Exercício 3:
-
-
-# celsius = float(input("Digite  Temperatura em Celsisus: "))
-
-# conversao = celsius * (9/5) + 32
-
-# print(f"{celsius}°C equivalem a {conversao}°F")
+# if num > 0:
+#     print("Esse número é positivo!")
+# elif num < 0:
+#     print("Esse número é negativo!")
+# elif num == 0: 
+#     print("Esse número é zero!")
 
 
 
 
 
+# Solicite a idade do usuário e informe em qual faixa etária ele se encontra: 
+ 
+#  Criança (0–12) 
+ 
+#  Adolescente (13–17) 
+ 
+#  Adulto (18–59) 
+ 
+#  Idoso (60+) 
+ 
+#  Dica: utilize múltiplos elif.
+
+
+
+# idade = int(input("Digite sua idade: "))
+
+# if idade >= 0 and idade <= 12:
+#     print("Você é criança")
+# elif idade >= 13 and idade <= 17:
+#     print("Você é Adolescente")
+# elif idade >= 18 and idade <= 59:
+#     print("Você é Adulto")
+# elif idade >= 60:
+#     print("Você é Idoso")
+
+
+
+# Crie um programa que calcule a média de três notas e determine a situação do aluno: 
+ 
+#  Média < 5 → Reprovado 
+ 
+#  5 ≤ Média < 7 → Recuperação 
+ 
+#  Média ≥ 7 → Aprovado 
+ 
+#  Desafio extra: adicione uma condição para validar se as notas são válidas (entre 0 e 10).
+
+nota1 = 7
+nota2 = 5
+nota3 = 9
+ 
+media = nota1 + nota2 + nota 3
 
 
