d08a8d7241b5186d920cbef9ba8e5a3ea37df498
Thiago Lima <thiagomlima2008@gmail.com>
2025-11-03 00:32:53 -0300

    pratica

--- PYTHON/praticando.py
+++ PYTHON/praticando.py
@@ -1,8 +1,8 @@
 # ATIVIDADE 1
-def boas_vindas():
-    print("Bem-vindo(a) ao sistema de cadastro de clientes! ")
+# def boas_vindas():
+#     print("Bem-vindo(a) ao sistema de cadastro de clientes! ")
 
-boas_vindas()
+# boas_vindas()
 
 
 
@@ -39,4 +39,43 @@ boas_vindas()
 
 
 
+
+# ATIVIDADE 4
+# Crie um programa completo chamado “Gestor de Produtos” usando funções. Requisitos: 
+ 
+#  Uma função menu() que mostre as opções:
+#    . Cadastrar produto
+#    . Listar produtos
+#    . Calcular valor total em estoque
+#    . Sair 
+ 
+#  Uma função cadastrar_produto(nome, preco, quantidade) que adicione um produto a uma lista. 
+ 
+#  Uma função listar_produtos() que mostre todos os produtos. 
+ 
+#  Uma função calcular_total() que retorne o valor total (preço × quantidade). 
+ 
+#  Use laços e estruturas condicionais para controlar o fluxo.
+
+
+
+def menu():
+    print("Gestor de Produtos")
+    print("1. Cadastrar produto")
+    print("2. Listar produtos")
+    print("3. Calcular valor total em estoque")
+    print("4. Sair")
+    return input("Escolha uma opção: ")
+
+def cadastrar_produto(produtos):
+    nome = input("Nome do produto: ")
+    preco = float(input("Preço do produto: "))
+    quantidade = int(input("Quantidades do produto: "))
+    produtos.append({"nome": nome, "preco": preco, "quantidade": quantidade})
+
+def listar_produtos(produtos):
+    if not produtos:
+        print("Não a produtos cadastrado")
+    else:
+        print(f"Produto '{nome}' cadastrado com sucesso!")
 
